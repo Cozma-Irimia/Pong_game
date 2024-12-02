ee7b78062602538e29b8766a4508610688e80b3b
Cozma-Gheorghe Irimia <cozma-gheorghe.irimia@student.tuiasi.ro>
2024-12-03 01:36:26 +0200

    I modified the ball positions and the bottom edge so that the ball hits and rebounds to the right side.

--- Pong_game/Pong.py
+++ Pong_game/Pong.py
@@ -11,12 +11,13 @@ run = True
 #colors RGB
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 #ball
 radius = 15
 ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
-ball_velocity_x = 1
-ball_velocity_y = 1
+ball_velocity_x = 0.7
+ball_velocity_y = 0.7
 
 #pozitii palete
 paddle_width = 20
@@ -27,9 +28,21 @@ right_paddle_x = WIDTH - (100 - paddle_width/2)
 
 #main loop
 while run:
+    wn.fill(BLACK)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+
+    #Miscarile controlate ale mingii
+    #verific daca mingea iese in afara ferestrei
+    if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
+        ball_velocity_y *= -1
+     
+
+    #Miscari
+    ball_x += ball_velocity_x
+    ball_y += ball_velocity_y
+
 
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
